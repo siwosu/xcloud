e77083a923e97e39fa6cb44d4e00a266972fc783
siwosu <118746154+siwosu@users.noreply.github.com>
2024-05-25 22:09:37 +0300

    Update registration.cpp

--- registration.cpp
+++ registration.cpp
@@ -77,11 +77,11 @@ registration::registration(QWidget *parent)
     // Устанавливаем соединение с базой данных, если оно еще не установлено
     if (!QSqlDatabase::contains("qt_sql_default_connection")) {
         db = QSqlDatabase::addDatabase("QMYSQL");
-        db.setHostName("147.78.65.249"); // host name
+        db.setHostName(""); // host name
         db.setPort(3306); // port
-        db.setDatabaseName("cs_test"); // name db
-        db.setUserName("Klimenko"); // login
-        db.setPassword("DCIfmTJFyy"); // password
+        db.setDatabaseName(""); // name db
+        db.setUserName(""); // login
+        db.setPassword(""); // password
 
         // Проверяем, удалось ли открыть соединение с базой данных
         if (!db.open()) {
